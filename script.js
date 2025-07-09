2ca26df95755c6d6a3bc2ecc7c9b4d4f42149a76
Vincent506 <vicenteneto0806@gmail.com>
2025-07-08 21:18:21 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Olá Mundo!!")
+const apiKeyInput = document.getElementById("ApiKey")
+const gameSelect = document.getElementById("gameSelect")
+const questionInput = document.getElementById("questionInput")
+const askButton = document.getElementById("askButton")
+const form = document.getElementById("form")
+const aiResponse = document.getElementById("aiResponse")
+
+const sendForm = (event) => {
+    event.preventDefaul()
+    console.log(event)
+}
+
+form.addEventListener('submit', sendForm)
