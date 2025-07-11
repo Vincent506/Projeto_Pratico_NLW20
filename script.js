c0c96806af3ecc177bd70995e7143e1e6af57379
Vincent506 <vicenteneto0806@gmail.com>
2025-07-11 10:58:22 -0300

    Update 1.0

--- script.js
+++ script.js
@@ -14,6 +14,30 @@ const sendForm = (event) => {
     const question = questionInput.value
 
     console.log({apiKey, game, question})
+
+    if (apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha TODOS os campos!!!')
+        return
+    }
+
+    askButton.disabled == true
+
+    askButton.textContent = 'Perguntando....'
+
+    askButton.classList.add('loading')
+
+    try {
+        //perguntar para IA
+
+    }catch(error) {
+        conseole.log('Error: ', error)
+
+    } finally {
+        askButton.disabled == false;
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove('loading')
+    }
+    
 }
 
 form.addEventListener('submit', sendForm)
